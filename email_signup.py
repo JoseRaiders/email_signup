958e94e23032333fb9753da32021eabfbd9c91e2
JoseRaiders <71295774+JoseRaiders@users.noreply.github.com>
2023-01-05 17:03:29 -0600

    Added the Mailchimp API

--- email_signup.py
+++ email_signup.py
@@ -2,6 +2,7 @@
 # 2. Ask for email address
 # 3. Validate email is correct with @ and .
 # 4. If True, go to MailChimp account and trigger a Welcome email (API call)
+# API key: 9aa12571048fe85bd0636b43eff87edf-us18
 
 import re
 
@@ -23,3 +24,17 @@ if email:
   print(email_address)
 else:
   print('Looks like you haven\'t submitted an email address')
+
+import mailchimp_marketing as MailchimpMarketing
+from mailchimp_marketing.api_client import ApiClientError
+
+try:
+  client = MailchimpMarketing.Client()
+  client.set_config({
+    "api_key": "9aa12571048fe85bd0636b43eff87edf-us18",
+    "server": "us18"
+  })
+  response = client.ping.get()
+  print(response)
+except ApiClientError as error:
+  print(error)
